34a85e9323b3a122dd0cfb0c4fd8478f6069ecf8
Beni1803 <beni1803@usherbrooke.ca>
2023-10-30 16:02:44 -0400

    tabs organization IS+MIRA

--- public/scripts.js
+++ public/scripts.js
@@ -59,7 +59,7 @@ function loadDashboard(url) {
 }
 
 function loadTabContents() {
-    const tabs = ['home', 'arcgis', 'webapp', 'excel', 'rfp', 'urls', 'data', 'templates'];
+    const tabs = ['home', 'arcgis', 'webapp', 'networksdata', 'rfp', 'economicsdata', 'templates'];
     tabs.forEach(tab => {
         fetch(`${tab}_tab_content.html`)
             .then(response => {
