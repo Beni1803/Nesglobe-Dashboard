e18aea97e2903922edaf0859a1ef759c64cd6043
mirasul98 <148999493+mirasul98@users.noreply.github.com>
2023-11-15 14:23:31 -0500

    database for economics tab

--- public/firestore.js
+++ public/firestore.js
@@ -68,9 +68,6 @@ function deleteRow(category, rowIndex) {
         console.error("Error deleting row:", error);
     });
 }
-// --------------------------------------------------------------------
-//         Economics and network ressources database handling
-// --------------------------------------------------------------------
 
 // Function to load network resources from Firestore and display them
 function loadNetworkResources(docType) {
@@ -182,4 +179,120 @@ function deleteResource(index, docType) {
 document.addEventListener('DOMContentLoaded', () => {
     loadNetworkResources('networks_excel_resources');
     loadNetworkResources('networks_online_resources');
+});
+
+// --------------------------------------------------------------------
+//         Economics resources database handling
+
+// Function to load economic resources from Firestore and display them
+function loadEconomicResources(docType) {
+    db.collection("networksdata").doc(docType).get().then((doc) => {
+        let content = '';
+        if (doc.exists) {
+            const resourcesArray = doc.data().resources;
+            if (resourcesArray && Array.isArray(resourcesArray)) {
+                resourcesArray.forEach((resource, index) => {
+                    content += `
+                        <div class="sharepoint-link">
+                            <a href="${resource.link}" target="_blank" rel="noopener noreferrer">
+                                <i class="fas fa-external-link-alt"></i> ${resource.label}
+                            </a>
+                            <i class="fas fa-edit" onclick="openEconomicsModal('edit', ${index}, '${docType}')"></i>
+                            <i class="fas fa-trash-alt" onclick="deleteResource(${index}, '${docType}')"></i>
+                        </div>
+                    `;
+                });
+            }
+        } else {
+            console.log(`No such document: ${docType}`);
+        }
+        document.getElementById(docType + 'Container').innerHTML = content;
+    }).catch((error) => {
+        console.log("Error getting document:", error);
+    });
+}
+
+// Function to open modal for adding or editing a resource
+function openEconomicsModal(mode, index, docType) {
+    // Reset the form values
+    document.getElementById('economicsLabel').value = '';
+    document.getElementById('economicsLink').value = '';
+    document.getElementById('economicsId').value = '';
+    document.getElementById('economicsDocType').value = docType; // Store the document type
+
+    if (mode === 'edit') {
+        // Load the current data into the modal fields
+        db.collection("networksdata").doc(docType).get().then((doc) => {
+            if (doc.exists) {
+                const resource = doc.data().resources[index];
+                document.getElementById('economicsLabel').value = resource.label;
+                document.getElementById('economicsLink').value = resource.link;
+                document.getElementById('economicsId').value = index;
+            }
+        });
+    }
+
+    $('#economicsModal').modal('show');
+}
+
+// Function to save a new or edited resource
+function saveEconomics() {
+    const label = document.getElementById('economicsLabel').value;
+    const link = document.getElementById('economicsLink').value;
+    const index = document.getElementById('economicsId').value;
+    const docType = document.getElementById('economicsDocType').value; // Retrieve the document type
+
+    const resource = { label, link };
+
+    if (index !== '') {
+        // Edit an existing resource
+        db.collection("networksdata").doc(docType).get().then((doc) => {
+            if (doc.exists) {
+                let resourcesArray = doc.data().resources;
+                resourcesArray[index] = resource;
+                return db.collection("networksdata").doc(docType).update({resources: resourcesArray});
+            }
+        }).then(() => {
+            loadEconomicResources(docType);
+            $('#economicsModal').modal('hide');
+        }).catch((error) => {
+            console.error("Error updating resource:", error);
+        });
+    } else {
+        // Add a new resource
+        db.collection("networksdata").doc(docType).update({
+            resources: firebase.firestore.FieldValue.arrayUnion(resource)
+        }).then(() => {
+            loadEconomicResources(docType);
+            $('#economicsModal').modal('hide');
+        }).catch((error) => {
+            console.error("Error adding resource:", error);
+        });
+    }
+}
+
+// Function to delete a resource with verification
+function deleteEconomics(index, docType) {
+    // Confirmation dialog
+    if (confirm("Are you sure you want to delete this source?")) {
+        db.collection("networksdata").doc(docType).get().then((doc) => {
+            if (doc.exists) {
+                let resourcesArray = doc.data().resources;
+                resourcesArray.splice(index, 1);
+                return db.collection("networksdata").doc(docType).update({resources: resourcesArray});
+            }
+        }).then(() => {
+            loadEconomicResources(docType);
+        }).catch((error) => {
+            console.error("Error deleting resource:", error);
+        });
+    } else {
+        console.log("Deletion cancelled.");
+    }
+}
+
+// Call loadEconomicResources for both types on page load
+document.addEventListener('DOMContentLoaded', () => {
+    loadEconomicResources('Economics_canadian_data');
+    loadEconomicResources('Economics_international_data');
 });
